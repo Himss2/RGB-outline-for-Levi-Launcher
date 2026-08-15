14c045cc96324481e2ac3495105b3a1264acdf60
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-15 13:16:40 +0700

    Create Version.hpp

---
+++ include/outline/Version.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string_view>
+
+namespace OutlineRGB {
+
+inline constexpr std::string_view Name =
+    "OutlineRGB";
+
+inline constexpr std::string_view Author =
+    "OutlineRGB Contributors";
+
+inline constexpr std::string_view Description =
+    "Standalone RGB 3D outline renderer for Minecraft Bedrock Android.";
+
+inline constexpr std::string_view Version =
+    "0.1.0";
+
+}
