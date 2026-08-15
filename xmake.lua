be05eb9bee6f53d4a73200218d8d7ee7d225c951
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-15 20:21:43 +0700

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -3,22 +3,33 @@ set_version("0.1.0")
 
 set_languages("cxx20")
 
-add_rules("mode.debug", "mode.release")
+add_rules(
+    "mode.debug",
+    "mode.release"
+)
 
 target("outlinergb")
     set_kind("shared")
     set_basename("outlinergb")
 
     add_files("src/*.cpp")
-    add_includedirs("include")
+
+    add_includedirs(
+        "include",
+        {public = true}
+    )
 
     add_cxxflags(
         "-Wall",
         "-Wextra",
         "-Wpedantic",
+        "-fvisibility=hidden",
         {force = true}
     )
 
     if is_plat("android") then
-        add_syslinks("log", "dl")
-    end
+        add_syslinks(
+            "log",
+            "dl"
+        )
+end
