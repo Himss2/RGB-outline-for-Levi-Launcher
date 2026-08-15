2da19735daef443eb3ea64bd6a6def5474124a7d
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-15 09:13:58 +0700

    Create config.hpp

---
+++ include/outline/config.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace OutlineConfig {
+
+inline bool enabled = true;
+
+// MVP: merah solid terlebih dahulu.
+inline float red   = 1.0f;
+inline float green = 0.0f;
+inline float blue  = 0.0f;
+inline float alpha = 1.0f;
+
+// Nanti dipakai setelah renderer terbukti.
+inline float thickness = 0.025f;
+
+// RGB nanti.
+inline bool rgb = false;
+inline float rgbSpeed = 1.0f;
+
+}
