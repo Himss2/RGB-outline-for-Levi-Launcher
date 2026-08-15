53ffe025a7966b004a982c9bd7419f7f49a74aef
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-15 20:15:24 +0700

    Update config.hpp

--- include/outline/config.hpp
+++ include/outline/config.hpp
@@ -1,20 +1,27 @@
 #pragma once
 
-namespace OutlineConfig {
+#include <atomic>
 
-inline bool enabled = true;
+namespace outline {
 
-// MVP: merah solid terlebih dahulu.
-inline float red   = 1.0f;
-inline float green = 0.0f;
-inline float blue  = 0.0f;
-inline float alpha = 1.0f;
+struct Config {
+    std::atomic_bool enabled{true};
 
-// Nanti dipakai setelah renderer terbukti.
-inline float thickness = 0.025f;
+    std::atomic_bool rgb{true};
 
-// RGB nanti.
-inline bool rgb = false;
-inline float rgbSpeed = 1.0f;
+    std::atomic<float> red{1.0f};
+    std::atomic<float> green{0.0f};
+    std::atomic<float> blue{0.0f};
+    std::atomic<float> alpha{1.0f};
+
+    std::atomic<float> rgbSpeed{1.0f};
+
+    std::atomic<float> thickness{0.025f};
+};
+
+inline Config& config() {
+    static Config value;
+    return value;
+}
 
 }
