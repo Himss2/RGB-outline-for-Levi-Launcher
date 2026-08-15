3d043790b5c7e431046517e9903a917eed70a355
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-15 20:16:01 +0700

    Update types.hpp

--- include/outline/types.hpp
+++ include/outline/types.hpp
@@ -2,20 +2,36 @@
 
 #include <cstdint>
 
+namespace outline {
+
 struct Vec3 {
-    float x;
-    float y;
-    float z;
+    float x{};
+    float y{};
+    float z{};
 };
 
 struct AABB {
-    Vec3 min;
-    Vec3 max;
+    Vec3 min{};
+    Vec3 max{};
 };
 
 struct Color {
-    float r;
-    float g;
-    float b;
-    float a;
+    float r{};
+    float g{};
+    float b{};
+    float a{};
 };
+
+struct ModuleRange {
+    std::uintptr_t base{};
+    std::uintptr_t textBegin{};
+    std::uintptr_t textEnd{};
+
+    bool valid() const {
+        return base != 0 &&
+               textBegin != 0 &&
+               textEnd > textBegin;
+    }
+};
+
+}
