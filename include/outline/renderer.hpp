541a572af1f884b1f76da792cbd787644af2e548
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-16 09:09:21 +0700

    Update renderer.hpp

--- include/outline/renderer.hpp
+++ include/outline/renderer.hpp
@@ -10,7 +10,7 @@ using Tessellator = void;
 using ScreenContext = void;
 
 using TessellatorBeginFn =
-    void (*)(Tessellator*, int);
+    void (*)(Tessellator*, void*, int, int, int);
 
 using TessellatorColorFn =
     void (*)(Tessellator*, float, float, float, float);
@@ -19,28 +19,11 @@ using TessellatorVertexFn =
     void (*)(Tessellator*, float, float, float);
 
 using RenderMeshImmediatelyFn =
-    void (*)(ScreenContext*, void*);
+    void (*)(ScreenContext*, Tessellator*, void*, char*);
 
 bool initialize();
-
 bool ready();
 
-void setTessellator(
-    Tessellator* tessellator
-);
-
-void begin(
-    int mode
-);
-
-void color(
-    const Color& color
-);
-
-void vertex(
-    const Vec3& position
-);
-
-void end();
+void renderSelection(void* levelRenderer, void* screenContext);
 
 }
