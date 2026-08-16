81b6501f5623e98a0b6d2288ba4ab23cf6d3ff26
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-16 09:21:51 +0700

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,5 @@
 set_project("OutlineRGB")
-set_version("0.1.0")
+set_version("0.2.0")
 
 set_languages("cxx20")
 
@@ -7,6 +7,22 @@ add_rules(
     "mode.debug",
     "mode.release"
 )
+
+-- LeviLauncher/Preloader provides the Android ARM64 inline-hook backend
+-- used by native Levi mods. The mod remains a single .levipack containing
+-- only libOutlineRGB.so; preloader is a host-side runtime dependency.
+package("preloader")
+    set_homepage("https://github.com/LiteLDev/preloader-android")
+    set_description("Preloader Android")
+    add_urls("https://github.com/LiteLDev/preloader-android.git")
+    add_versions("main", "main")
+    add_deps("cmake")
+    on_install("android", function(package)
+        import("package.tools.cmake").install(package)
+    end)
+package_end()
+
+add_requires("preloader")
 
 target("outlinergb")
     set_kind("shared")
@@ -18,6 +34,8 @@ target("outlinergb")
         "include",
         {public = true}
     )
+
+    add_packages("preloader")
 
     add_cxxflags(
         "-Wall",
@@ -32,4 +50,4 @@ target("outlinergb")
             "log",
             "dl"
         )
-end
+    end
