8ca5a89056d609bcee5e6ef77ad25786bc98e7fa
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-16 10:58:15 +0700

    Update resolver.hpp

--- include/outline/resolver.hpp
+++ include/outline/resolver.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <cstddef>
 #include <cstdint>
 #include <string_view>
 #include <vector>
@@ -13,17 +12,29 @@ struct Candidate {
 };
 
 bool initialize(std::string_view libraryName);
+
 bool ready();
 
 std::uintptr_t libraryBase();
+
 std::uintptr_t renderLevel();
+
 std::uintptr_t clientInstanceUpdate();
+
 std::uintptr_t clientInstanceGetLocalPlayer();
+
 std::uintptr_t levelGetHitResult();
+
 std::uintptr_t tessellatorBegin();
+
 std::uintptr_t tessellatorColor();
+
 std::uintptr_t tessellatorVertex();
+
 std::uintptr_t meshRenderImmediately();
+
+std::uintptr_t meshRenderImmediately2();
+
 std::uintptr_t blockGetOutline();
 
 const std::vector<Candidate>& blockOutlineCandidates();
