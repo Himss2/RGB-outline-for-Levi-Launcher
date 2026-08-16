a78adb2a1b628205d55490fce58b31a5e291aba6
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-16 09:06:45 +0700

    Create runtime.cpp

---
+++ src/runtime.cpp
@@ -0,0 +1,19 @@
+#include "outline/runtime.hpp"
+
+#include <atomic>
+
+namespace outline::runtime {
+
+namespace {
+std::atomic<void*> gClientInstance{nullptr};
+}
+
+void setClientInstance(void* value) {
+    gClientInstance.store(value, std::memory_order_release);
+}
+
+void* clientInstance() {
+    return gClientInstance.load(std::memory_order_acquire);
+}
+
+}
